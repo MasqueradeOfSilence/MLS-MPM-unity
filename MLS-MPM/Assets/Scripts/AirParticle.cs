9f59a8bfbfc0e3acd6c30331c89d77f3f76d22e0
Alex <alexcn711@gmail.com>
2023-06-16 09:13:05 -0600

    fixed AirParticle

--- MLS-MPM/Assets/Scripts/AirParticle.cs
+++ MLS-MPM/Assets/Scripts/AirParticle.cs
@@ -2,17 +2,10 @@ using Unity.Mathematics;
 
 public class AirParticle : Particle
 {
-    private double2 position;
-    private double2 velocity;
     private double mass = 0.5;
-    // The affine momentum matrix C
-    private double2x2 affineMomentumMatrix;
-
     public void InitParticle(double2 position, double2 velocity, double2x2 affineMomentumMatrix)
     {
-        this.position = position;
-        this.velocity = velocity;
-        this.affineMomentumMatrix = affineMomentumMatrix;
+        InitParticle(position, velocity, mass, affineMomentumMatrix);
     }
 
     public new double GetMass()
