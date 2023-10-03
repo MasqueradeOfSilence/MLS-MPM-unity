fe3df61744718ad1493181451e4a2eaa9e251f19
Alex <alexcn711@gmail.com>
2023-10-02 19:22:36 -0600

    triangulation progress

--- MLS-MPM/Assets/Editor/FluidSurfacerTests.cs
+++ MLS-MPM/Assets/Editor/FluidSurfacerTests.cs
@@ -8,10 +8,12 @@ public class FluidSurfacerTests
     [Test]
     public void InitializePolygonShouldCreateAPolygonBasedOnASetOfPoints()
     {
+        int expectedNumPoints = 4096;
         FoamSimulator foamSimulator = GameObject.Find("ExampleGeo").AddComponent<FoamSimulator>();
         foamSimulator.InitializeFoamSimulator();
         foamSimulator.InitializeParticlesWithFluidAtBottom();
         Polygon polygon = FluidSurfacer.InitializePolygon(foamSimulator.GetParticles());
         Assert.IsNotNull(polygon);
+        Assert.AreEqual(polygon.Points.Count, expectedNumPoints);
     }
 }
