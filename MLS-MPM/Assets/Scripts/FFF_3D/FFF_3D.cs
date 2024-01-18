957f2af5700492df2bd79193e933ca32bf664602
Alex <alexcn711@gmail.com>
2024-01-18 16:37:08 -0700

    Beginning FFF 3D

---
+++ MLS-MPM/Assets/Scripts/FFF_3D/FFF_3D.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * FFF_3D: Simulates a 3D foam with a small pool of water at the bottom, using the Foam Fraction Flow method. 
+ */
+
+public class FFF_3D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
