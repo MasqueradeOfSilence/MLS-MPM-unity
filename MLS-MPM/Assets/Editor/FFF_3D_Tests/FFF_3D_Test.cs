caba51e681a33cab3bea4612557bbef837195461
Alex <alexcn711@gmail.com>
2024-01-20 16:00:37 -0700

    Progress on 3D-ifying

--- MLS-MPM/Assets/Editor/FFF_3D_Tests/FFF_3D_Test.cs
+++ MLS-MPM/Assets/Editor/FFF_3D_Tests/FFF_3D_Test.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
-public class FFF_3D_Test : MonoBehaviour
+[TestFixture]
+public class FFF_3D_Test 
 {
-    // Start is called before the first frame update
-    void Start()
+    [Test]
+    public void InitShouldSetUpGridAndParticles()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- MLS-MPM/Assets/Scripts/FFF_3D/FFF_3D.cs
+++ MLS-MPM/Assets/Scripts/FFF_3D/FFF_3D.cs
@@ -13,6 +13,12 @@ public class FFF_3D : MonoBehaviour
      * Data members
      */
     private Particle_3D[][][] particles;
+    private Grid_3D[] grid;
+    private int gridResolution = 64;
+    private const double timestep = 0.2;
+    private const int numberOfSimulationsPerUpdateCall = (int) (1 / timestep);
+    private const double gravity = -9.8;
+
 
     // Start is called before the first frame update
     void Start()
