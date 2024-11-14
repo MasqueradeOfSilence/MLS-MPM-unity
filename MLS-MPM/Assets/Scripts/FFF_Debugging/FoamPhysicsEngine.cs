b060f77fde6ebf54ae56a5fb13f4a38153d32df4
Alex <alexcn711@gmail.com>
2024-11-13 18:11:15 -0700

    test engine wip

---
+++ MLS-MPM/Assets/Scripts/FFF_Debugging/FoamPhysicsEngine.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * In this class, I will slowly build up the non-Voronoi pieces of FFF 
+ *  until I ascertain the source of the right-trending divergence.
+ */
+
+public class FoamPhysicsEngine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
