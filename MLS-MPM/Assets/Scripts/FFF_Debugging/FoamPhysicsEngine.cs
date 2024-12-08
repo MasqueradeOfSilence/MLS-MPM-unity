8c1fdef392d433320803d92273ee8c5ac513405a
Alex <alexcn711@gmail.com>
2024-12-07 17:33:39 -0700

    we are going to have to multithread for reasonable testing speed

--- MLS-MPM/Assets/Scripts/FFF_Debugging/FoamPhysicsEngine.cs
+++ MLS-MPM/Assets/Scripts/FFF_Debugging/FoamPhysicsEngine.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 /**
  * In this class, I will slowly build up the non-Voronoi pieces of FFF 
  *  until I ascertain the source of the right-trending divergence.
+ *  
+ *  This will entail testing it for quite a few frames, which means that multithreading this time is imperative. 
  */
 
 public class FoamPhysicsEngine : MonoBehaviour
