7bd275f2d58debf812fce32feeeea3275310d46a
Alex <alexcn711@gmail.com>
2024-12-13 20:49:41 -0700

    wip on debug

---
+++ MLS-MPM/Assets/Scripts/FFF_Debugging/Objects/Particle_Debug.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+public class Particle_Debug
+{
+    private double3 position;
+    private double3 velocity;
+    private double mass;
+    private double3x3 affineMomentumMatrix_C;
+}
