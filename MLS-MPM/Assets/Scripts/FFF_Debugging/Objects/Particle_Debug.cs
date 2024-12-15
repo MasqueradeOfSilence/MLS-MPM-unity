9f23da050b5f20751e7c60707da0716b3fe5e7c2
Alex <alexcn711@gmail.com>
2024-12-14 17:20:53 -0700

    progress on particles

--- MLS-MPM/Assets/Scripts/FFF_Debugging/Objects/Particle_Debug.cs
+++ MLS-MPM/Assets/Scripts/FFF_Debugging/Objects/Particle_Debug.cs
@@ -9,4 +9,12 @@ public class Particle_Debug
     private double3 velocity;
     private double mass;
     private double3x3 affineMomentumMatrix_C;
+
+    public Particle_Debug(double3 position, double3 velocity, double mass, double3x3 c) 
+    { 
+        this.position = position;
+        this.velocity = velocity;
+        this.mass = mass;
+        this.affineMomentumMatrix_C = c;
+    }
 }
