211c4e0c5389b822e2cf858f7b2a5fab552cb2e6
MasqueradeOfSilence <alexcn711@gmail.com>
2025-01-01 18:13:02 -0800

    adding unit tests for fff debug

---
+++ MLS-MPM/Assets/Editor/FFF_3D_Tests/FFF_Debug_Tests/FoamPhysicsEngine_Debug_Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class FoamPhysicsEngine_Debug_Test
+{
+	[Test]
+	void InitializeFluid()
+	{
+
+	}
+
+	[Test]
+	void InitializeGrid()
+	{
+
+	}
+
+	[Test]
+	void InitializeParticles()
+	{
+
+	}
+}
+
