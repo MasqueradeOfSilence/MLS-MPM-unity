a91d806886febb16584e4c73234196bcd3ec18e7
Alex <alexcn711@gmail.com>
2025-01-17 19:34:14 -0700

    renaming variables for equations

--- MLS-MPM/Assets/Scripts/FFF_Debugging/Objects/Particle_Debug.cs
+++ MLS-MPM/Assets/Scripts/FFF_Debugging/Objects/Particle_Debug.cs
@@ -8,13 +8,13 @@ public class Particle_Debug
     private double3 position;
     private double3 velocity;
     private double mass;
-    private double3x3 affineMomentumMatrix_C;
+    private double3x3 C_affineMomentumMatrix;
 
     public Particle_Debug(double3 position, double3 velocity, double mass, double3x3 c) 
     { 
         this.position = position;
         this.velocity = velocity;
         this.mass = mass;
-        this.affineMomentumMatrix_C = c;
+        this.C_affineMomentumMatrix = c;
     }
 }
