c19ef274e8a30d793cc6a1714241a578f2255a99
Alex <alexcn711@gmail.com>
2025-01-24 18:36:23 -0700

    cell class

---
+++ MLS-MPM/Assets/Scripts/FFF_Debugging/Objects/Cell_Debug.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+public class Cell_Debug 
+{
+    private double3 velocity;
+    private double mass;
+}
