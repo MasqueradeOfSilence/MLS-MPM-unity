053fb79df08b4957430c1457cfa0f03c3033627a
Alex <alexcn711@gmail.com>
2025-04-12 17:05:37 -0600

    gravity adjust for full fluid-only sim

--- MLS-MPM/Assets/Scripts/FFF_3D/FFF_Optimized_3D.cs
+++ MLS-MPM/Assets/Scripts/FFF_3D/FFF_Optimized_3D.cs
@@ -33,7 +33,7 @@ public class FFF_Optimized_3D : MonoBehaviour
     private readonly int resolution = 16; // Was 64 for 2D
     private const double timestep = 0.2;
     private const int numSimsPerUpdate = (int)(1 / timestep);
-    private const double gravity = -9.8;
+    private const double gravity = -0.3;
     private int neighborDimension = 3;
     private GameInterface_3D gameInterface;
     private WaterSurfacer_3D waterSurfacer;
@@ -90,7 +90,6 @@ public class FFF_Optimized_3D : MonoBehaviour
             // allFoam = no fluid particles. Name needs to be changed inside of Game Interface.
             allFoam = true;
         }
-        // TODO start here -- we need to build a default mode to render opaque particles that are slightly larger
         gameInterface.DumpParticlesIntoScene(particles, shouldUseFFFShader, shouldUseWhiteShader, allFoam, shouldUseParticleOnlyShader); 
         gameInterface.NukeClones();
         if (!alembicEnabled)
@@ -430,31 +429,32 @@ public class FFF_Optimized_3D : MonoBehaviour
             int eosPower = 2;
             double smallestValue = double.MaxValue;
             // Mapping
-            for (int l = 0; l < 3; l++)
-            {
-                for (int m = 0; m < 3; m++)
-                {
-                    double currentValue = strain[l][m];
-                    if (currentValue < smallestValue)
-                    {
-                        smallestValue = currentValue;
-                    }
-                }
-            }
+            /*            for (int l = 0; l < 3; l++)
+                        {
+                            for (int m = 0; m < 3; m++)
+                            {
+                                double currentValue = strain[l][m];
+                                if (currentValue < smallestValue)
+                                {
+                                    smallestValue = currentValue;
+                                }
+                            }
+                        }
+                        double extraOffset = 0;
+                        if (smallestValue < 0)
+                        {
+                            extraOffset = 0.001;
+                            smallestValue = math.abs(smallestValue);
+                            extraOffset += smallestValue;
+                            for (int row = 0; row < 3; row++)
+                            {
+                                for (int col = 0; col < 3; col++)
+                                {
+                                    strain[row][col] += extraOffset;
+                                }
+                            }
+                        }*/
             double extraOffset = 0;
-            if (smallestValue < 0)
-            {
-                extraOffset = 0.001;
-                smallestValue = math.abs(smallestValue);
-                extraOffset += smallestValue;
-                for (int row = 0; row < 3; row++)
-                {
-                    for (int col = 0; col < 3; col++)
-                    {
-                        strain[row][col] += extraOffset;
-                    }
-                }
-            }
 
             // TODO is this strain modification necessary if using Newtonian
             
