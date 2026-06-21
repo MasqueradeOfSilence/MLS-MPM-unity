101acbebb39796eca5dc399b2d0f0f86e82fc193
MasqueradeOfSilence <alexcn711@gmail.com>
2026-06-20 21:35:53 -0600

    WIP

--- MLS-MPM/Assets/Scripts/FFF_3D/FFF_Optimized_3D.cs
+++ MLS-MPM/Assets/Scripts/FFF_3D/FFF_Optimized_3D.cs
@@ -40,6 +40,7 @@ public class FFF_Optimized_3D : MonoBehaviour
     private bool onlySimOnce = false; // Set to TRUE only for debug
     private bool haveSimmedOnce = false; // No touchy
     // TODO revisit how the alembic was working
+    // it's not currently on point
     // simply setting this to true does not show alembics - wrong path? 
     private bool renderWater = false;
     private bool alembicEnabled = false;
